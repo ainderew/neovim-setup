5aac705102a0a92ef15400160a6346b36c6d7d28
Andrew Pinon <61579578+ainderew@users.noreply.github.com>
2022-11-30 13:26:10 +0800

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("andrew.plugins-setup")
+require("andrew.core.options")
+require("andrew.core.keymaps")
+require("andrew.core.colorscheme")
---
+++ nvim/lua/andrew/core/keymaps.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+--genral keymaps
+
+keymap.set("i", "jk", "<ESC>")
